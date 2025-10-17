8816473a8f63e3a0ee02ebb8c731bd72aa6aaa8d
amjadfaiz-ma <amuhamadafandy@ucsd.edu>
2025-10-17 10:34:58 -0700

    add current

--- global.js
+++ global.js
@@ -1,5 +1,17 @@
-console.log('IT’S ALIVE!');
+console.log("IT'S ALIVE!");
 
+// Helper function to select multiple elements
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// Step 2.1: Get an array of all nav links
+const navLinks = $$("nav a");
+
+// Step 2.2: Find the link to the current page
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+// Step 2.3: Add the "current" class safely
+currentLink?.classList.add("current");
